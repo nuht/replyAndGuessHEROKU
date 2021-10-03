4f8bb3c1aa764a639533a56ea929a130ac535be5
PARY Quentin <paryquentin@MacBook-Pro-de-PARY.local>
2021-10-03 14:53:59 +0200

    installation de REACT OK

---
+++ assets/app.js
@@ -0,0 +1,20 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+function App() {
+    return <h1>Hello World !</h1>;
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
