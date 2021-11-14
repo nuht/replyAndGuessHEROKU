54290daa41d6bd020bf4f6fe42f3b2ce5bb72806
paryquentin <q.pary@outlook.fr>
2021-11-14 15:19:26 +0100

    export surveycard component to his file

---
+++ assets/components/SurveyList/SurveyCard.js
@@ -0,0 +1,38 @@
+import {Button, Card, CardActions, CardContent, Stack, Typography} from "@mui/material";
+import {Link} from "react-router-dom";
+import {SurveyStatus} from "../SurveyStatus";
+import React from "react";
+import PropTypes from "prop-types";
+
+SurveyCard.propTypes = {
+    survey: PropTypes.shape({
+        id:PropTypes.string,
+        status:PropTypes.string,
+        title:PropTypes.string,
+    }).isRequired,
+    toggleSurveyStatus: PropTypes.func.isRequired
+}
+
+export function SurveyCard(props) {
+    return (
+        <Card sx={{minWidth: 275}}>
+            <Link to={"/survey/" + props.survey.id} style={{
+                textDecoration: 'none',
+                color: '#333333'
+            }}>
+                <CardContent>
+                    <Stack direction="row" spacing={2} alignItems="center">
+                        <SurveyStatus status={props.survey.status}/>
+                        <Typography variant="h5" gutterBottom>
+                            {props.survey.title}
+                        </Typography>
+                    </Stack>
+                </CardContent>
+            </Link>
+            <CardActions>
+                <Button onClick={props.toggleSurveyStatus}
+                        size="small">{props.survey.status === 'waiting' ? 'Activer' : 'Désactiver'}</Button>
+            </CardActions>
+        </Card>
+    )
+}
--- assets/components/SurveyList/SurveyList.js
+++ assets/components/SurveyList/SurveyList.js
@@ -1,41 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {Link} from "react-router-dom";
-import {SurveyStatus} from "../SurveyStatus";
-import {Button, Card, CardActions, CardContent, Typography, Stack} from "@mui/material";
+import {SurveyCard} from "./SurveyCard";
+
 SurveyList.propTypes = {
     history: PropTypes.object.isRequired,
     match: PropTypes.object.isRequired,
     location: PropTypes.object.isRequired
 };
-
-SurveyCard.propTypes = {
-    survey: PropTypes.object.isRequired,
-    toggleSurveyStatus: PropTypes.func.isRequired
-}
-
-export function SurveyCard(props) {
-    return (
-        <Card sx={{ minWidth: 275 }}>
-            <Link to={"/survey/" + props.survey.id} style={{
-                textDecoration: 'none',
-                color: '#333'
-            }}>
-                <CardContent>
-                    <Stack direction="row" spacing={2} alignItems="center">
-                        <SurveyStatus status={props.survey.status} />
-                        <Typography variant="h5" gutterBottom>
-                            {props.survey.title}
-                        </Typography>
-                    </Stack>
-                </CardContent>
-            </Link>
-            <CardActions>
-                <Button onClick={props.toggleSurveyStatus} size="small">{props.survey.status === 'waiting' ? 'Activer' : 'Désactiver'}</Button>
-            </CardActions>
-        </Card>
-    )
-}
 
 export function SurveyList(props) {
     const [surveyList, setSurveyList] = React.useState(null);
@@ -66,7 +37,6 @@ export function SurveyList(props) {
             {surveyList !== null && <ul>{
                 surveyList.map(survey => {
                     return <SurveyCard key={survey.id} survey={survey} toggleSurveyStatus={() => {
-
                         toggleSurveyStatus(survey.id, survey.status)
                     }}/>
                 })
